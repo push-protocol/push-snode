14ccbfd26531580c131ad43ad2cec69e6b908a45
Archisman Das <dasarchisman25@gmail.com>
2023-01-10 14:24:51 +0530

    add db healthcheck and update readme

--- snode/src/appInit.ts
+++ snode/src/appInit.ts
@@ -31,37 +31,37 @@ async function startServer(logLevel = null) {
   const app = express();
   const server = require("http").createServer(app);
 
-  const Pool = require("pg").Pool;
+  // const Pool = require("pg").Pool;
 
-  const credentials = {
-    user: "postgres",
-    host: "db",
-    database: "snode1",
-    password: "postgres",
-    port: 5432,
-  };
+  // const credentials = {
+  //   user: "postgres",
+  //   host: "db",
+  //   database: "snode1",
+  //   password: "postgres",
+  //   port: 5432,
+  // };
 
-  let retries = 5;
+  // let retries = 5;
 
-  async function connect() {
+  // async function connect() {
     
-    const pool = new Pool(credentials);
-    const client = await pool.connect();
-    console.log("Connected to database");
-    client.release();
-  }
+  //   const pool = new Pool(credentials);
+  //   const client = await pool.connect();
+  //   console.log("Connected to database");
+  //   client.release();
+  // }
 
-  while (retries) {
-    try {
-      await connect();
-      break;
-    } catch (err) {
-      console.log(err);
-      retries -= 1;
-      console.log(`retries left: ${retries}`);
-      // wait 5 seconds
-      await new Promise((res) => setTimeout(res, 5000));
-    }}
+  // while (retries) {
+  //   try {
+  //     await connect();
+  //     break;
+  //   } catch (err) {
+  //     console.log(err);
+  //     retries -= 1;
+  //     console.log(`retries left: ${retries}`);
+  //     // wait 5 seconds
+  //     await new Promise((res) => setTimeout(res, 5000));
+  //   }}
 
 
 
