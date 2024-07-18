1ed8e5662ddf372be7169d20ee08d3b8480e2573
aman035 <guptaaman200115@gmail.com>
2024-07-18 17:15:47 +0530

    test: husky

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,34 +1,32 @@
-import express from 'express';
-const bodyParser = require('body-parser');
-const cors = require('cors');
-import routes from '../api/index';
-import config from '../config/index';
+import express from 'express'
+const bodyParser = require('body-parser')
+const cors = require('cors')
+import routes from '../api/index'
 export default ({ app }: { app: express.Application }) => {
-
   app.get('/status', (req, res) => {
-
-    res.status(200).end();
-  });
+    res.status(200).end()
+  })
   app.head('/status', (req, res) => {
-    res.status(200).end();
-  });
+    //
+    res.status(200).end()
+  })
 
   // The magic package that prevents frontend developers going nuts
   // Alternate description:
   // Enable Cross Origin Resource Sharing to all origins by default
-  app.use(cors());
+  app.use(cors())
 
-  app.use('/api', routes());
+  app.use('/api', routes())
 
   // Load Static Files
   // app.use(express.static(config.staticServePath));
 
   /// catch 404 and forward to error handler
   app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err['status'] = 404;
-    next(err);
-  });
+    const err = new Error('Not Found')
+    err['status'] = 404
+    next(err)
+  })
 
   /// error handlers
   app.use((err, req, res, next) => {
@@ -36,20 +34,16 @@ export default ({ app }: { app: express.Application }) => {
      * Handle 401 thrown by express-jwt library
      */
     if (err.name === 'UnauthorizedError') {
-      return res
-        .status(err.status)
-        .send({ message: err.message })
-        .end();
+      return res.status(err.status).send({ message: err.message }).end()
     }
-    return next(err);
-  });
+    return next(err)
+  })
   app.use((err, req, res) => {
-    res.status(err.status || 500);
+    res.status(err.status || 500)
     res.json({
       error: {
         info: err.message
       }
-    });
-  });
-
-};
+    })
+  })
+}
