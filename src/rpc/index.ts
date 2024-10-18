d043d5c4fee7800de3de2053cea677b826f3d9ad
Igx22 <swein2@gmail.com>
2024-10-18 21:48:46 +0700

    fix: renamed methods to be consistent with vnode!

--- src/rpc/index.ts
+++ src/rpc/index.ts
@@ -1,20 +1,22 @@
 // import the cpntrolers from the controllers folder
 
+// TODO convert this to a single rpc object???
+
 import { GetAccountInfo } from './get_accountInfo'
 import { StorageGetTransaction } from './storage_getTransaction'
 import { StorageGetTransactions } from './storage_getTransactions'
 // put the controllers here
 const controllers = {
-  storage_getTransactions: StorageGetTransactions.storageGetTransactions,
+  push_getTransactions: StorageGetTransactions.storageGetTransactions,
   storage_getTransaction: StorageGetTransaction.storageGetTransaction,
-  get_accountInfo: GetAccountInfo.getAccountInfo
+  push_accountInfo: GetAccountInfo.getAccountInfo
 }
 
 // put the after test controllers here, keep in mind to keep the function name same as controller
 const afterController = {
-  storage_getTransactions: StorageGetTransactions.afterStorageGetTransactions,
+  push_getTransactions: StorageGetTransactions.afterStorageGetTransactions,
   storage_getTransaction: StorageGetTransaction.afterStorageGetTransaction,
-  get_accountInfo: GetAccountInfo.afterGetAccountInfo
+  push_accountInfo: GetAccountInfo.afterGetAccountInfo
 }
 
 export const rpcControllerConfigs = {
