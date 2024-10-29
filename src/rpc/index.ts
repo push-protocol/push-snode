6f04c6e5489c9be9f549838183dc94acc54d43b1
Ashis <pradhanashish95@gmail.com>
2024-10-29 16:49:49 +0530

    fix: some best practices for rpc classes

--- src/rpc/index.ts
+++ src/rpc/index.ts
@@ -9,23 +9,13 @@ import { StorageGetTransaction } from './storage_getTransaction'
 import { StorageGetTransactions } from './storage_getTransactions'
 // put the controllers here
 const controllers = {
-  push_getTransactions: StorageGetTransactions.storageGetTransactions,
-  storage_getTransaction: StorageGetTransaction.storageGetTransaction,
-  push_accountInfo: GetAccountInfo.getAccountInfo,
-  push_putBlockHash: PushPutBlockHash.pushPutBlockHash,
-  push_putBlock: PushPutBlock.pushPutBlock
-}
-
-// put the after test controllers here, keep in mind to keep the function name same as controller
-const afterController = {
-  push_getTransactions: StorageGetTransactions.afterStorageGetTransactions,
-  storage_getTransaction: StorageGetTransaction.afterStorageGetTransaction,
-  push_accountInfo: GetAccountInfo.afterGetAccountInfo,
-  push_putBlockHash: PushPutBlockHash.afterPushPutBlockHash,
-  push_putBlock: PushPutBlock.afterPushPutBlock
+  push_getTransactions: new StorageGetTransactions().execute,
+  storage_getTransaction: new StorageGetTransaction().execute,
+  push_accountInfo: new GetAccountInfo().execute,
+  push_putBlockHash: new PushPutBlockHash().execute,
+  push_putBlock: new PushPutBlock().execute
 }
 
 export const rpcControllerConfigs = {
-  controllers,
-  afterController
+  controllers
 }
